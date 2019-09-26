22e38a4548c6a931ccb295e05308057787394306
Wildhevire <wildhevire404@gmail.com>
2019-09-26 17:52:37 +0000

    finished

---
+++ src/latihan/logika/Logika.java
@@ -0,0 +1,16 @@
+package latihan.logika;
+
+public class Logika {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : Mendeklarasikan dan implementasi operasi Logika
+     */
+	public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+}
